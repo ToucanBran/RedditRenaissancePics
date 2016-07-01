06485a7856f138be071e23b97b06b10b59b21b00
ToucanBran <brandon.gomez89@gmail.com>
2016-06-30 20:05:23 -0700

    Added more code to download pics from the subreddit. Code works...for the most part. It's able to grab the top posts except some of the files don't download all the way(?) and also it doesn't seem to support some of the links (i.redd.it). I'll need to fix those later and also add comments to the code. Like I said though, it does work!

--- downloadRenaissance.py
+++ downloadRenaissance.py
@@ -3,15 +3,32 @@
 #downloads posts from /r/accidentalrenaissance
 
 import requests, os, bs4, logging, sys, re
+from enum import IntEnum
 
 
 logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 
+class Upvotes(IntEnum):
+	LOW = 1
+	MEDIUM = 500
+	HIGH = 900
+
 def download_pic(link_array):
+	logging.info(link_array)
 	os.makedirs('renaissance', exist_ok=True)
-	req = requests.get(link_array[0])
-	req.raise_for_status()
-	image = open(os.path.join('renaissance',))
+
+	for link in link_array:
+		try:
+			req = requests.get(link)
+			req.raise_for_status()
+			image = open(os.path.join('renaissance',os.path.basename(req.url)),'wb')
+			for chunk in req.iter_content(chunk_size=1024):
+				image.write(chunk)
+			image.close()
+		except requests.exceptions.MissingSchema:
+			continue
+
+
 
 	
 
@@ -19,6 +36,7 @@ def download_pic(link_array):
 
 
 pic_list = []
+
 #gets webpage
 req = requests.get('https://www.reddit.com/r/accidentalrenaissance')
 req.raise_for_status()
@@ -27,13 +45,14 @@ reddit_soup = bs4.BeautifulSoup(req.text,"html.parser")
 
 linkElements = reddit_soup.find_all(class_="score likes")
 
-logging.info(len(linkElements))
-logging.info(linkElements[5].text)
-logging.info(linkElements[5].parent.parent.attrs['data-url'])
+logging.debug(len(linkElements))
+logging.debug(linkElements[5].text)
+logging.debug(linkElements[5].parent.parent.attrs['data-url'])
 
 for link in linkElements:
-	if link.text != "\u2022" and int(link.text) > 1000:
+	if link.text != "\u2022" and int(link.text) > Upvotes.LOW:
 		logging.debug(link.text)
+		logging.debug(link.parent.parent.attrs['data-url'])
 		pic_list.append(link.parent.parent.attrs['data-url'])
 
 logging.info(pic_list)
